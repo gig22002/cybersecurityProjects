9c365a051b7fa52dcdf56ceebefde13c5899bb29
gio <gianpaolo.girasoli@uconn.edu>
2026-07-16 16:31:19 -0400

    created simple js payload adder

---
+++ reverseEngineering/pdfMal/main.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+from pypdf import PdfReader, PdfWriter
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        f = sys.argv[1]
+    else: sys.exit("Missing pdf")
+
+    r = PdfReader(f)
+    page = r.pages[0]
+
+    out = PdfWriter()
+    out.add_page(page)
+    #out.add_js("<script>alert(1)</script>")
+    out.add_js("app.alert('You have been pwned. Enjoy this virus!', 3);")
+
+    with open("out.pdf", "wb") as file:
+        out.write(file)
