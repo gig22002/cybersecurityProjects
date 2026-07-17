ef981c2c8369727c59584a39734ecadd5bb1df4e
gio <gianpaolo.girasoli@uconn.edu>
2026-07-17 08:10:40 -0400

    removed metadata

--- reverseEngineering/pdfMal/main.py
+++ reverseEngineering/pdfMal/main.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     page = r.pages[0]
 
     out = PdfWriter()
+    out.metadata = None
     out.add_page(page)
     #out.add_js("<script>alert(1)</script>")
     out.add_js("app.alert('You have been pwned. Enjoy this virus!', 3);")
