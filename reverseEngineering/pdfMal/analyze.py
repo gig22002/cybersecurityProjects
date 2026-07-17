734b6ed321b470c926bf583ed09a63a3b7c7c9a8
gio <gianpaolo.girasoli@uconn.edu>
2026-07-17 08:53:00 -0400

    added more metadata capability and content reading

--- reverseEngineering/pdfMal/analyze.py
+++ reverseEngineering/pdfMal/analyze.py
@@ -12,13 +12,24 @@ if __name__ == "__main__":
     page = r.pages[0]
 
     meta = r.metadata
+    xmeta = r.xmp_metadata
 
-    print("=== Metadata ===")
-    print(meta.title)
-    print(meta.author)
-    print(meta.subject)
-    print(meta.creator)
-    print(meta.producer)
-    print(meta.creation_date)
-    print(meta.modification_date)
+    print("=== Overview ===")
+    print(f"File Name: {sys.argv[1]}")
+    print("Content:\n"+page.extract_text())
 
+
+    print("\n=== Metadata ===")
+    print(f"Title: {meta.title}")
+    print(f"Author: {meta.author}")
+    print(f"Subject: {meta.subject}")
+    print(f"Creator: {meta.creator}")
+    print(f"Producer: {meta.producer}")
+    print(f"Creation Date: {meta.creation_date}")
+    print(f"Modification Date: {meta.modification_date}")
+    print(f"\nXMP Title: {xmeta.dc_title}")
+    print(f"XMP Description: {xmeta.dc_description}")
+    print(f"XMP Date: {xmeta.xmp_create_date}")
+    print(f"XMP Creator: {xmeta.dc_creator}")
+    print(f"PDF Producer: {xmeta.pdf_producer}")
+    print(f"PDF Version: {xmeta.pdf_pdfversion}")
